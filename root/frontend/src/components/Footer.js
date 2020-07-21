c63d69acebc7e8c964a99ac88c250ce06fb2bb47
nicolethorley <thorleynicole@gmail.com>
2020-07-21 15:09:45 +0100

    added footer

---
+++ root/frontend/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+const FooterPage = () => {
+  return (
+    <MDBFooter className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Front Row Co</h5>
+            <p>An unforgettable movie experience.</p>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright{" "}
+          <a href="#"> frontrowco.com </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+};
+
+export default FooterPage;
