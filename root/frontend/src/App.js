898a507f128daa7ebed9673e2318037810b5b8fb
tayyibah906 <tayyibah906@gmail.com>
2020-07-21 17:15:11 +0100

    quickbook feature added

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -4,15 +4,17 @@ import Header from "./PageComponents/Header";
 import HomepageCarousel from "./components/HomepageCarousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import NavBar from "./components/NavBar";
+import NavBar from "./PageComponents/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FooterPage from "./components/Footer";
+import Quickbook from "./PageComponents/quickbook"
 
 function App() {
   return (
     <div class="page-body">
       <Header />
       <NavBar />
+      <Quickbook/>
       <HomepageCarousel />
       <FooterPage />
     </div>
