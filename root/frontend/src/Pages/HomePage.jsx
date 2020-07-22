eab15edba918657d6faa0658eb8a8613a4883fb9
tayyibah906 <tayyibah906@gmail.com>
2020-07-22 15:33:28 +0100

    added about section

--- root/frontend/src/Pages/HomePage.jsx
+++ root/frontend/src/Pages/HomePage.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import {Container, Button } from "react-bootstrap";
 
 
 
 export default function HomePage() {
     return (
         <div>
-            
-            
+        <Container>
+  <h1 style={{ color: 'white' }}>Welcome To The Front Row Co.</h1>
+  <p style={{ color: 'white' }}>
+  ourscreen allows film fans to watch the films they love at our cinema, 
+  whether that’s a cult classic, foreign thriller or modern masterpiece.
+  Fans can start by booking their tickets to screenings. 
+  For first-time fans, this page is designed to help you navigate the waters 
+  to enjoying a screening and booking a ticket. 
+  </p>
+  <p>
+    <Button variant="danger">Contact Us</Button>
+  </p>
+</Container>
         </div>
     );
 }
