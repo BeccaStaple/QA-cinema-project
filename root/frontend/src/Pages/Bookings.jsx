dc5c1f8e1d056a8259a5a88178080cf3106c5fff
Becca Staple <petite.becca.s@gmail.com>
2020-07-22 16:32:25 +0100

    added payment form not functioning

--- root/frontend/src/Pages/Bookings.jsx
+++ root/frontend/src/Pages/Bookings.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BookingForm from "../components/BookingForm";
+import PaymentForm from "../components/PaymentForm";
 
 export default function Bookings() {
     return (
         <div>
             <BookingForm/>
+            <PaymentForm/>
         </div>
     );
 }
