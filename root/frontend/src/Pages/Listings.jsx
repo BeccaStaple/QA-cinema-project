274f1ca4b1834eb71acaccef88a2532fc1e27602
Becca Staple <petite.becca.s@gmail.com>
2020-07-24 14:02:31 +0100

    added cards

--- root/frontend/src/Pages/Listings.jsx
+++ root/frontend/src/Pages/Listings.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../index.css';
 import Axios from "axios";
+import { Container, CardDeck, Card } from "react-bootstrap";
 
 export default class Listings extends React.Component {
   constructor(props) {
@@ -12,25 +13,43 @@ export default class Listings extends React.Component {
 
   componentDidMount() {
     Axios.get("http://localhost:9090/cinema/movies").then((res) => {
-    const movies = res.data;
-    this.setState({ movies });
+      const movies = res.data;
+      this.setState({ movies });
     });
   }
 
+
+
   render() {
-    const movieTitles = this.state.movies.map(movie => {
+    
+    const showMovies = this.state.movies.map(movie => {
       return (
-      <div>
-        <p>Title: {movie.title}</p>
-        <img src={movie.movie_image} />
-      </div>
+        <div>
+          <Container>
+            <CardDeck>
+
+              <Card  >
+                <Card.Body >
+                  <p>Title: {movie.title}</p>
+                  <img src={movie.movie_image} className="w-100"/>
+                </Card.Body>
+
+              </Card>
+
+            </CardDeck>
+          </Container>
+        </div>
       )
-      
+
     })
     return (
       <div>
-        {movieTitles}
+        
+                  {showMovies}
+                
+
       </div>
     );
   }
 }
+
