5be81d91fd85028f8762e345b02b54d9181c5eaf
nicolethorley <thorleynicole@gmail.com>
2020-07-27 16:24:36 +0100

    completed endpoints for getting table data from screens and ticket types

--- root/backend/routes/apiRoutes.js
+++ root/backend/routes/apiRoutes.js
@@ -2,8 +2,13 @@ const apiRouter = require("express").Router();
 
 const movieRouter = require("./movieRouter");
 const bookingsRouter = require("./bookingsRouter");
+const screenRouter = require("./screenRouter");
+const ticketTypeRouter = require("./ticketTypeRouter");
 
 apiRouter.use("/movies", movieRouter);
+apiRouter.use("/screens", screenRouter);
+apiRouter.use("/ticketTypes", ticketTypeRouter);
+
 apiRouter.use("/bookings", bookingsRouter);
 
 module.exports = apiRouter;
