924bac3a8e7bb73a5024cbe63c3ab5dfb5488746
nicolethorley <thorleynicole@gmail.com>
2020-07-28 09:55:26 +0100

    added second test to movies

--- root/backend/test/movieTest.js
+++ root/backend/test/movieTest.js
@@ -5,9 +5,18 @@ const { describe } = require("mocha");
 const request = require("supertest");
 
 describe("movies", () => {
-  it("GET: All movies to include movie details", () => {
+  it("GET: returns an array", () => {
     return request(app)
       .get("/cinema/movies")
+      .expect(200)
+      .then((res) => {
+        expect(res.body).to.be.an("array");
+      });
+  });
+  it("GET: all movies to include movie details", () => {
+    return request(app)
+      .get("/cinema/movies")
+      .expect(200)
       .then((res) => {
         const movie = res.body[0];
         expect(movie).to.have.all.keys(
