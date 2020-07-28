64084cafc70fdc7e6a74c11f7ab1032333d31160
nicolethorley <thorleynicole@gmail.com>
2020-07-28 10:12:27 +0100

    get for movietime starttime where movie id =1

--- root/backend/controllers/movieController.js
+++ root/backend/controllers/movieController.js
@@ -1,5 +1,3 @@
-// import models
-const { selectMovies } = require("../models/movieModel");
 const connection = require("../models/db");
 
 const getMovies = async (req, res, next) => {
@@ -8,4 +6,13 @@ const getMovies = async (req, res, next) => {
   });
 };
 
-module.exports = getMovies;
+const getStartTimeByID1 = async (req, res, next) => {
+  connection.query(
+    "SELECT start_time FROM MovieTime WHERE fk_movie_id = 1;",
+    function (err, results, fields) {
+      return res.json(results);
+    }
+  );
+};
+
+module.exports = { getMovies, getStartTimeByID1 };
--- root/backend/routes/movieRouter.js
+++ root/backend/routes/movieRouter.js
@@ -1,7 +1,11 @@
 const movieRouter = require("express").Router();
 
-const getMovies = require("../controllers/movieController");
+const {
+  getMovies,
+  getStartTimeByID1,
+} = require("../controllers/movieController");
 
 movieRouter.get("/", getMovies);
+movieRouter.get("/startTimesByID1", getStartTimeByID1);
 
 module.exports = movieRouter;
